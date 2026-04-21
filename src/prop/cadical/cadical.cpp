b69c8f1214d89588f3268942a71926c00db204e4
Florian Pollitt <pollittf@informatik.uni-freiburg.de>
2026-04-21 15:38:00 +0200

    verbose mode

--- src/prop/cadical/cadical.cpp
+++ src/prop/cadical/cadical.cpp
@@ -15,7 +15,8 @@
 #include "prop/cadical/cadical.h"
 
 #include <cadical/cadical.hpp>
-#include <cadical/tracer.hpp>
+#include <cstdint>
+#include <memory>
 
 #include "base/check.h"
 #include "options/base_options.h"
@@ -26,12 +27,12 @@
 #include "prop/cadical/util.h"
 #include "prop/sat_solver_types.h"
 #include "prop/theory_proxy.h"
+#include "theory/shared_terms_database.h"
 #include "util/resource_manager.h"
 #include "util/statistics_registry.h"
 #include "util/string.h"
 
 namespace cvc5::internal::prop {
-
 using namespace cadical;
 
 /* -------------------------------------------------------------------------- */
@@ -91,40 +92,22 @@ void CadicalSolver::initialize()
   // walk and lucky phase do not use the external propagator, disable for now
   if (d_propagator)
   {
-    d_solver->set("report", 1);
     d_solver->set("walk", 0);
     d_solver->set("lucky", 0);
-    d_solver->set("stats", 1);
-
+    d_solver->set("report", 1);
+    d_solver->set("stats", 3);
     d_solver->connect_fixed_listener(d_propagator.get());
     d_solver->connect_external_propagator(d_propagator.get());
+    d_solver->build(stdout, "c ");
+    d_solver->options();
   }
   else
     d_solver->set("quiet", 1);  // CaDiCaL is verbose by default
-
-  d_solver->build(stdout, "c ");
-  d_solver->options();
 
   d_true = newVar(false, true);
   d_false = newVar(false, true);
   d_solver->clause(toCadicalVar(d_true));
   d_solver->clause(-toCadicalVar(d_false));
-
-  bool logProofs = false;
-  // TODO (wishue #154): determine how to initialize the proofs for CaDiCaL
-  // here based on d_env.isSatProofProducing and options().proof.propProofMode.
-  // The latter should be extended to include modes DRAT and LRAT based on
-  // what is available here.
-  if (logProofs)
-  {
-    d_pfFile = options().driver.filename + ".drat_proof.txt";
-    if (!options().proof.dratBinaryFormat)
-    {
-      d_solver->set("binary", 0);
-    }
-    d_solver->set("inprocessing", 0);
-    d_solver->trace_proof(d_pfFile.c_str());
-  }
 }
 
 CadicalSolver::~CadicalSolver()
@@ -142,7 +125,7 @@ CadicalSolver::~CadicalSolver()
 class ResourceLimitTerminator : public CaDiCaL::Terminator
 {
  public:
-  ResourceLimitTerminator(ResourceManager& resmgr) : d_resmgr(resmgr) {};
+  ResourceLimitTerminator(ResourceManager& resmgr) : d_resmgr(resmgr) {}
 
   bool terminate() override
   {
@@ -194,32 +177,13 @@ SatValue CadicalSolver::_solve(const std::vector<SatLiteral>& assumptions)
     d_propagator->in_search(true);
   }
   res = toSatValue(d_solver->solve());
-  d_solver->statistics();
   if (d_propagator)
   {
+    d_solver->statistics();
     Assert(res != SAT_VALUE_TRUE || d_propagator->done());
     Trace("cadical::propagator") << "solve done: " << res << std::endl;
     d_propagator->in_search(false);
   }
-#ifndef NDEBUG
-  // Check unsat core
-  if (res == SAT_VALUE_FALSE && d_proof_tracer != nullptr)
-  {
-    std::vector<SatClause> unsat_core;
-    d_proof_tracer->compute_unsat_core(unsat_core, true);
-
-    std::unique_ptr<CaDiCaL::Solver> solver(new CaDiCaL::Solver());
-    for (const auto& clause : unsat_core)
-    {
-      for (const auto& lit : clause)
-      {
-        solver->add(toCadicalLit(lit));
-      }
-      solver->add(0);
-    }
-    Assert(solver->solve() == CaDiCaL::UNSATISFIABLE);
-  }
-#endif
   ++d_statistics.d_numSatCalls;
   d_inSatMode = (res == SAT_VALUE_TRUE);
   return res;
@@ -425,35 +389,8 @@ std::shared_ptr<ProofNode> CadicalSolver::getProof()
   {
     ProofNodeManager* pnm = d_env.getProofNodeManager();
     NodeManager* nm = d_env.getNodeManager();
-
-    std::vector<SatClause> unsat_core;
-    d_proof_tracer->compute_unsat_core(unsat_core);
-
-    std::vector<std::shared_ptr<ProofNode>> ps;
-    for (const auto& sat_clause : unsat_core)
-    {
-      NodeBuilder nb(nm, Kind::OR);
-      std::vector<Node> lits;
-      for (const auto& lit : sat_clause)
-      {
-        lits.push_back(d_proxy->getNode(lit));
-      }
-      // Sat clause is sorted by literal id. Ensure that node-level clause is
-      // sorted by node ids.
-      std::sort(lits.begin(), lits.end());
-      for (const auto& lit : lits)
-      {
-        nb << lit;
-      }
-      Node n = nb.getNumChildren() == 1 ? nb[0] : nb.constructNode();
-      ps.push_back(pnm->mkAssume(n));
-    }
-    return pnm->mkNode(ProofRule::SAT_REFUTATION, ps, {});
+    return d_proof_tracer->get_chain_resolution_proof(pnm, nm, d_proxy);
   }
-  // NOTE: we could return a DRAT_REFUTATION or LRAT_REFUTATION proof node
-  // consisting of a single step, referencing the files for the DIMACS + proof.
-  // do not throw an exception, since we test whether the proof is available
-  // by comparing it to nullptr.
   return nullptr;
 }
 
