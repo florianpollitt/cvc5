3699124b589734a185876e735ddbd9dbb5c2fdf1
Florian Pollitt <pollittf@informatik.uni-freiburg.de>
2026-05-11 08:25:59 +0200

    hash_sum + notify_fixed

--- src/prop/cadical/cdclt_propagator.cpp
+++ src/prop/cadical/cdclt_propagator.cpp
@@ -108,7 +108,7 @@ void CadicalPropagator::notify_fixed_assignment(int lit)
     return;
   }
   // TODO: cadical needs to notify backtrack before notify_fixed...
-  // return;
+  return;
   ++d_stats.notifyFixedAssignment;
 
   Trace("cadical::propagator")
