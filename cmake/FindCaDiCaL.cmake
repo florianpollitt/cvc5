fe5ef889cf835ccbb6634cd1d828daccf1024014
Florian Pollitt <pollittf@informatik.uni-freiburg.de>
2026-06-02 08:08:51 +0200

    decide new

--- cmake/FindCaDiCaL.cmake
+++ cmake/FindCaDiCaL.cmake
@@ -84,19 +84,18 @@ if(NOT CaDiCaL_FOUND_SYSTEM)
   include(CheckSymbolExists)
   include(ExternalProject)
 
-  set(CaDiCaL_VERSION "development")
-  set(CaDiCaL_CHECKSUM "4ff6de630fc1c64564b96463408b960bfe828db443f658acd6e7bea02402a730")
-
   # avoid configure script and instantiate the makefile manually the configure
   # scripts unnecessarily fails for cross compilation thus we do the bare
   # minimum from the configure script here
   
-  # Release Build
-  set(CaDiCaL_CXXFLAGS "-fPIC -O3 -DNDEBUG -std=c++11")
+  set(CaDiCaL_VERSION "development")
+  set(CaDiCaL_CHECKSUM "55c62cfe8fa6b6553524543c7ab59d4483eb144f442606025ff6a3085d439c88")
   # Debug Build
   # set(CaDiCaL_CXXFLAGS "-fPIC -DLOGGING -std=c++11")
+  # Release Build
+  #set(CaDiCaL_CXXFLAGS "-fPIC -O3 -DNDEBUG -std=c++11")
   # Quiet Build
-  #set(CaDiCaL_CXXFLAGS "-fPIC -O3 -DNDEBUG -DQUIET -std=c++11")
+  set(CaDiCaL_CXXFLAGS "-fPIC -O3 -DNDEBUG -DQUIET -std=c++11")
   
   if(CMAKE_CROSSCOMPILING_MACOS)
     set(CaDiCaL_CXXFLAGS "${CaDiCaL_CXXFLAGS} -arch ${CMAKE_OSX_ARCHITECTURES}")
