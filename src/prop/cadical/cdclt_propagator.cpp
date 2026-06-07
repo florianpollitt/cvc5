60489848522945cc933133771fc6d5128b055f1f
Florian Pollitt <pollittf@informatik.uni-freiburg.de>
2026-06-07 14:20:04 +0200

    decide all - no fixed listener

--- src/prop/cadical/cdclt_propagator.cpp
+++ src/prop/cadical/cdclt_propagator.cpp
@@ -106,7 +106,7 @@ void CadicalPropagator::notify_fixed_assignment(int lit)
   {
     return;
   }
-  // return;
+  return;
   ++d_stats.notifyFixedAssignment;
 
   Trace("cadical::propagator")
@@ -336,6 +336,7 @@ int CadicalPropagator::cb_decide()
     */
     else
       lit = d_proxy->getNextDecisionRequest(requirePhase, stopSearch);
+    // return 0;
   }
   Trace("cadical::propagator") << "cb::decide: 0" << std::endl;
   return 0;
