528591b4d51cb3ca845d2cfd215810bf463d29ac
Florian Pollitt <pollittf@informatik.uni-freiburg.de>
2026-06-19 10:38:40 +0200

    cmake and decision heuristic

--- cmake/FindCaDiCaL.cmake
+++ cmake/FindCaDiCaL.cmake
@@ -89,13 +89,13 @@ if(NOT CaDiCaL_FOUND_SYSTEM)
   # minimum from the configure script here
   
   set(CaDiCaL_VERSION "development")
-  set(CaDiCaL_CHECKSUM "252ecbb2c40770e7c8e29840b7a7b63fb637c60d0192640b7b4e732325eb7347")
+  set(CaDiCaL_CHECKSUM "279032bf5a36c575cbff2d99cedf988fd1bcf499562c5b8ca12696b27842bcb5")
   # Debug Build
   # set(CaDiCaL_CXXFLAGS "-fPIC -DLOGGING -std=c++11")
   # Release Build
-  set(CaDiCaL_CXXFLAGS "-fPIC -O3 -DNDEBUG -std=c++11")
+  #set(CaDiCaL_CXXFLAGS "-fPIC -O3 -DNDEBUG -std=c++11")
   # Quiet Build
-  #set(CaDiCaL_CXXFLAGS "-fPIC -O3 -DNDEBUG -DQUIET -std=c++11")
+  set(CaDiCaL_CXXFLAGS "-fPIC -O3 -DNDEBUG -DQUIET -std=c++11")
   
   if(CMAKE_CROSSCOMPILING_MACOS)
     set(CaDiCaL_CXXFLAGS "${CaDiCaL_CXXFLAGS} -arch ${CMAKE_OSX_ARCHITECTURES}")
--- src/prop/cadical/cdclt_propagator.cpp
+++ src/prop/cadical/cdclt_propagator.cpp
@@ -106,7 +106,7 @@ void CadicalPropagator::notify_fixed_assignment(int lit)
   {
     return;
   }
-  return;
+  // return;
   ++d_stats.notifyFixedAssignment;
 
   Trace("cadical::propagator")
@@ -330,9 +330,10 @@ int CadicalPropagator::cb_decide()
       if (d_solver.force_unassign(toCadicalLit(lit))) return toCadicalLit(lit);
       return 0;
     }
-    */
     else
       lit = d_proxy->getNextDecisionRequest(requirePhase, stopSearch);
+    */
+    return 0;
   }
   Trace("cadical::propagator") << "cb::decide: 0" << std::endl;
   return 0;
