96cda16d28d1c37b0ccefc305244a877c0ed2029
Thomas Hader <18447861+Ovascos@users.noreply.github.com>
2026-08-15 13:17:36 +0000

    Fixed CadicalSolver propagation only mode with assumptions. (#12809)
    By setting the decision limit to `0` in `CaDiCaL::setPropagateOnly`
    assumptions were ignored on the next solving call. Internally, CaDiCaL
    treats assumptions as decisions. By setting the decisions limit to 0,
    assumptions are never applied.
    This PR changes the behavior to call `CaDiCaL::propagate()` instead of
    `CaDiCaL::solve` when requested; without limiting the number of
    decisions. With `CaDiCaL::propagate()` all assumptions are applied to
    the internal trail followed by a run of BCP without an actual decision.
    Thus unsatisfiability with assumptions is detected.

--- src/prop/cadical/cadical.cpp
+++ src/prop/cadical/cadical.cpp
@@ -79,10 +79,13 @@ CadicalSolver::CadicalSolver(Env& env,
     : EnvObj(env),
       d_solver(new CaDiCaL::Solver()),
       d_context(context()),
+      d_propagateOnly(false),
       // Note: CaDiCaL variables start with index 1 rather than 0 since negated
       //       literals are represented as the negation of the index.
       d_nextVarIdx(1),
       d_inSatMode(false),
+      d_true(undefSatVariable),
+      d_false(undefSatVariable),
       d_statistics(registry, name)
 {
 }
@@ -162,7 +165,6 @@ SatValue CadicalSolver::_solve(const std::vector<SatLiteral>& assumptions)
       d_solver->assume(toCadicalLit(~lit));
     }
   }
-  SatValue res;
   for (const SatLiteral& lit : assumptions)
   {
     if (d_propagator)
@@ -176,7 +178,8 @@ SatValue CadicalSolver::_solve(const std::vector<SatLiteral>& assumptions)
   {
     d_propagator->in_search(true);
   }
-  res = toSatValue(d_solver->solve());
+  const SatValue res =
+      toSatValue(d_propagateOnly ? d_solver->propagate() : d_solver->solve());
   if (d_propagator)
   {
     d_solver->statistics();
@@ -185,6 +188,7 @@ SatValue CadicalSolver::_solve(const std::vector<SatLiteral>& assumptions)
     d_propagator->in_search(false);
   }
   ++d_statistics.d_numSatCalls;
+  d_propagateOnly = false;
   d_inSatMode = (res == SAT_VALUE_TRUE);
   return res;
 }
@@ -252,7 +256,7 @@ SatValue CadicalSolver::solve(const std::vector<SatLiteral>& assumptions)
 
 bool CadicalSolver::setPropagateOnly()
 {
-  d_solver->limit("decisions", 0); /* Gets reset after next solve() call. */
+  d_propagateOnly = true;
   return true;
 }
 
